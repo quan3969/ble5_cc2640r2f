6dfaa45e056b64eb80905c533baa4b0384cc65ea
Zhongquan Zheng <39052985+quan3969@users.noreply.github.com>
2019-11-21 16:42:46 +0800

    Revert "Go back."
    This reverts commit 275d29b5165195d6e252bab976453e6d807dd13b.

--- ble5_template/FlashROM_StackLibrary/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"C:/TI/simplelink_cc2640r2_sdk_3_30_00_20/source/ti/devices/cc26x0r2/driverlib/bin/ccs/driverlib.lib" -l"C:/TI/simplelink_cc2640r2_sdk_3_30_00_20/kernel/tirtos/packages/ti/dpl/lib/dpl_cc26x0r2.aem3" -l"C:/TI/simplelink_cc2640r2_sdk_3_30_00_20/source/ti/drivers/lib/drivers_cc26x0r2.aem3" -l"C:/TI/simplelink_cc2640r2_sdk_3_30_00_20/source/ti/drivers/rf/lib/rf_singleMode_cc26x0r2.aem3" -l"C:/TI/simplelink_cc2640r2_sdk_3_30_00_20/source/ti/display/lib/display.aem3" -l"C:/TI/simplelink_cc2640r2_sdk_3_30_00_20/source/ti/grlib/lib/ccs/m3/grlib.a" -l"C:/Users/quan3/workspace_v9_2/ble5_project_zero_cc2640r2lp_stack_library/FlashROM_Library/ble_r2.symbols" -l"C:/Users/quan3/workspace_v9_2/ble5_project_zero_cc2640r2lp_stack_library/FlashROM_Library/lib_linker.cmd" -l"C:/Users/quan3/workspace_v9_2/ble5_project_zero_cc2640r2lp_stack_library/FlashROM_Library/ble5_project_zero_cc2640r2lp_stack_library.lib" -l"C:/TI/simplelink_cc2640r2_sdk_3_30_00_20/source/ti/ble5stack/common/cc26xx/ccs/cc26xx_app.cmd" -llibc.a
-
